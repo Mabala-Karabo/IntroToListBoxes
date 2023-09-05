a4d4dcfd592e255dcebbf13d67bf51be32bad8fd
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-05 12:06:40 +0200

    Add project files.

---
+++ IntroToListBoxes/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnYourGender_Click(sender As Object, e As EventArgs) Handles btnYourGender.Click
+        Dim strYourGender As String
+
+        strYourGender = ListBox1.SelectedItem
+        MsgBox("Hi you are " + strYourGender)
+    End Sub
+End Class
